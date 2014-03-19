5ea0e60b03d206799999e0955919173616c9ea04
unknown <nicolas.husser@gmail.com>
2014-03-19 16:43:48 -0400

    Commiting project

---
+++ js/client.js
@@ -0,0 +1,201 @@
+(function($){
+
+	// Create connection to the socket
+	var socket    = io.connect('http://localhost:1337');
+	// Handle on the template to inject using Mustache.js
+	var msgtpl    = $('#msgtpl').html();
+					$('#msgtpl').remove();
+	// Array to keep track of all the usernames (autocheck)
+	var usernames = [];
+
+	// Variables to handle the user typing events
+	var typingTimer;                
+	var doneTypingInterval1 = 1;
+	var doneTypingInterval2 = 1000;
+	var userTaken = false;
+
+	// Keep track of myself
+	var self;
+
+	
+
+	// On form validation send emit the login event to the server
+	// It'll create a new user
+	$('#loginform').submit(function(event){
+		event.preventDefault();
+
+		// Proceed only if the username is not taken
+		if(!userTaken){
+
+			$('.global_container').animate({
+		    top: '-100%',
+			  }, 2000, function() {
+			});
+
+			setTimeout(function(){
+				$('#right_side').fadeIn();
+				$('#send_block').fadeIn();
+				}, 
+				2000
+			);
+
+			socket.emit('login', {
+				username: $('#username').val(),
+				mail	: $('#mail').val()
+			});
+
+			$('#login_block').remove();	
+			$('#message').focus();
+		}		
+
+	});
+
+
+	/*********************************************************************
+	 *					SENDING EVENTS TO THE SERVER
+	 *********************************************************************/
+	/*
+	 *	Manage the message sending
+	 */
+	 $('#sendMessage').submit(function(event){
+	 	event.preventDefault();
+	 	// Prevent user from sending empty strings
+	 	if(($('#message').val().match(/\S/))){
+
+	 		socket.emit('newMsg', {
+	 			message : $('#message').val()
+		 	});
+
+		 	$('#message').val('');
+		 	$('#message').focus();
+	 	}
+	 });
+
+
+	/*********************************************************************
+	 *					RECEIVING EVENTS & HANDLING THEM
+	 *********************************************************************/
+
+	/*
+	 *	Manage the usernames checking
+	 */
+	socket.on('usernamePassing', function(usernameArray){ usernames = usernameArray; });
+	/*
+	 *	Manage the user successful connection
+	 */
+	socket.on('logged', function(user){ self = user });
+	/*
+	 *	Receive the event saying that a user has typed
+	 */
+	socket.on('userTyping', function(user_name){   $('#is_writing').html(user_name+' is typing...')   });	
+	/*
+	 *	Receive the event saying that a user has finished typing
+	 */
+	socket.on('doneTyping', function(){   $('#is_writing').html('')   });	
+
+	/*
+	 *	Manage the message receiving
+	 */
+	 socket.on('newMsg', function(msg){
+	 	// Fill the template to display the message and its attributes
+	 	$('#messages').append(Mustache.render(msgtpl, msg)+'<br/>');
+	 	// Scroll to the bottom of the page to display newly sent message
+	 	$('#messages').animate({
+		    scrollTop: $("#messages")[0].scrollHeight,
+		  }, 500, function() {
+		});
+
+	 	// Update the action in the feed box
+		$('#feed_box ul').append('<li><i class="fa fa-comments-o"></i></i> &nbsp;'+msg.user.username+" posted a message</li>");
+		// Scroll to bottom of the feed box
+		$('#feed_box').scrollTop($('#feed_box').prop("scrollHeight"));
+	 });
+
+	/*
+	 *	Manage the user connection
+	 */
+	socket.on('newUser', function(user){
+		// Update the connection in the feed box
+		$('#feed_box ul').append('<li><i class="fa fa-arrow-circle-o-right connect"></i> &nbsp;'+user.username+" has connected</li>");
+		// Scroll to bottom of the feed box
+		$('#feed_box').scrollTop($('#feed_box').prop("scrollHeight"));
+		// Add the user from the list on the right nav
+		$('#users ul ').append('<li id="'+user.id+'"><img class="chat_avatar" src="'+user.avatar+'"><div class="chat_username">'+user.username+'</div><br/></li>');
+		// Update number of online users
+		$('#num_online').html(user.online_friends);
+	});
+
+	/*
+	 *	Manage the user disconnection
+	 */
+	socket.on('delUser', function(user){
+		// Update the disconnection in the feed box
+		$('#feed_box ul').append('<li><i class="fa fa-arrow-circle-o-left disconnect"></i> &nbsp;'+user.username+" has disconnected</li>");
+		// Scroll to bottom of the feed box
+		$('#feed_box').scrollTop($('#feed_box').prop("scrollHeight"));
+		// Remove the user from the list on the right nav
+		$('#'+user.id).remove();
+		// Update number of online users
+		$('#num_online').html(user.online_friends);
+	});
+
+
+
+
+	/*********************************************************************
+	 *					USER EVENTS (KEYPRESS,KEYUP,KEY DOWN)
+	 *********************************************************************/
+	/*
+	 *	Manage the user typing event
+	 */
+	$('#message').keypress(function(event){
+	    socket.emit('userTyping', {
+			user: self
+		});
+	});
+
+	/*
+	 *	Manage the username checking while the user is typing
+	 */
+	$('#message').keyup(function(){
+	    clearTimeout(typingTimer);
+	    typingTimer = setTimeout(doneTyping, doneTypingInterval2);
+	});
+	$('#message').keydown(function(){
+	    clearTimeout(typingTimer);
+	});
+	function doneTyping () {
+		socket.emit('doneTyping');
+	}
+
+	/*
+	 *	Manage the username checking while the user is typing
+	 */
+	$('#username').keyup(function(){
+	    clearTimeout(typingTimer);
+	    typingTimer = setTimeout(checkUsername, doneTypingInterval1);
+	});
+	$('#username').keydown(function(){
+	    clearTimeout(typingTimer);
+	});
+	function checkUsername () {
+		USERNAME = $('#username').val();
+
+	    for(var i=0; i<usernames.length; i++){
+			if(USERNAME == usernames[i]){
+				userTaken = true;
+				$('#user_check').css('color','red');
+				break;
+			}else{
+				$('#user_check').css('color','#fff');
+				userTaken = false;
+			}
+		}
+	}
+
+
+
+
+
+})(jQuery);
+
---
+++ server.js
@@ -0,0 +1,123 @@
+/*
+ *	Imports that we need: http, MD5, Socket.IO
+ */
+var http = require('http');
+var md5	 = require('MD5');
+
+/*
+ *	Create server and make it listen on port 1337
+ */
+httpServer = http.createServer(function(request, response) {
+  console.log('User connecting...');
+});
+httpServer.listen(1337);
+
+var io 			 = require('socket.io').listen(httpServer);
+// Will contain the unique ID of every online user
+var online_users = {};
+// Will contain the message objects of the previous conversation
+var message_hist = [];
+// Will limit the history so that the retrieval is fast
+var history		 = 20;
+// Will keep track of all the usernames
+var usernames    = [];
+// Keep track of the user who sent the last message
+var lastUserMessage = '';
+
+
+/*
+ *	Open socket
+ */
+io.sockets.on('connection', function(socket){
+
+	var self = false;
+	console.log('New user');
+
+	// This will display all the online users to the client
+	for(var user in online_users)
+		socket.emit('newUser', online_users[user]);
+
+	// This will dispay the history of previous users
+	for(var msg in message_hist){
+		socket.emit('newMsg', message_hist[msg]);
+	}
+
+	// Tell the client all the username already taken
+	socket.emit('usernamePassing', usernames);
+		
+
+	/*
+	 *	Message received
+	 */
+	 socket.on('newMsg', function(msg){
+	 	msg.user  = self;
+	 	date      = new Date();
+	 	msg.h     = date.getHours();
+	 	msg.m     = date.getMinutes();
+	 	
+	 	if(msg.m.toString().length == 1)
+	 		msg.m = "0"+msg.m;
+	 	
+	 	// Handle history
+	 	message_hist.push(msg);
+	 	if(message_hist > history)
+	 		message_hist.shift();
+
+	 	io.sockets.emit('newMsg', msg);
+
+	 });
+
+	 socket.on('doneTyping', function(){
+	 	socket.broadcast.emit('doneTyping');
+	 });
+
+	/*
+	 *	Manage the user typing event
+	 */
+	 socket.on('userTyping', function(user){
+	 	 socket.broadcast.emit('userTyping', user.user.username);
+	 });
+
+
+	/*
+	 *	User connection
+	 */
+	socket.on('login', function(user){
+		self = user;
+		// Create unique ID for the new user
+		self.id 	= user.mail.replace(/\./g, '_').replace('@','_');
+		// Create user avatar
+		self.avatar = 'https://gravatar.com/avatar/'+md5(user.mail)+'?s=32&d=identicon&r=PG';
+		// Tell the user he's logged in
+		socket.emit('logged', self);
+		// Add user to the list to display them all on login
+		online_users[self.id] = self;
+		// Pass the number of online users in user object
+		user.online_friends = Object.size(online_users);
+		// Add the username to the list
+		usernames.push(self.username);
+		// Send info to all the users
+		io.sockets.emit('newUser', user); 
+	});
+
+	/*
+	 *	User disconnection
+	 */
+	socket.on('disconnect', function(){
+		if(!self)
+			return false;
+
+		delete online_users[self.id];
+		self.online_friends = Object.size(online_users);
+		io.sockets.emit('delUser', self);
+	});
+
+});
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
